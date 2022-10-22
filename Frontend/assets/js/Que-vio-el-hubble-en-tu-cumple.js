e2ce4f34435eb2aea3d18144d32c5fe1cd47b5de
romy-rbo <oliva.romina@gmail.com>
2022-10-22 16:31:31 -0300

    Cambios finales sin footer

--- Frontend/assets/js/Que-vio-el-hubble-en-tu-cumple.js
+++ Frontend/assets/js/Que-vio-el-hubble-en-tu-cumple.js
@@ -1,38 +1,29 @@
-// Create a lightbox
-(function() {
-    var $lightbox = $("<div class='lightbox'></div>");
-    var $img = $("<img>");
-    var $caption = $("<p class='caption'></p>");
-  
-    // Add image and caption to lightbox
-  
-    $lightbox
-      .append($img)
-      .append($caption);
-  
-    // Add lighbox to document
-  
-    $('body').append($lightbox);
-  
-    $('.lightbox-gallery img').click(function(e) {
-      e.preventDefault();
-  
-      // Get image link and description
-      var src = $(this).attr("data-image-hd");
-      var cap = $(this).attr("alt");
-  
-      // Add data to lighbox
-  
-      $img.attr('src', src);
-      $caption.text(cap);
-  
-      // Show lightbox
-  
-      $lightbox.fadeIn('fast');
-  
-      $lightbox.click(function() {
-        $lightbox.fadeOut('fast');
-      });
-    });
-  
-  }());
+// INICIO Código lightbox imágenes //
+const images = document.querySelectorAll('.gallery-img')
+const containerImage = document.querySelector('.image-lightbox')
+const imageContainer = document.querySelector('.img-show')
+const imageTitle = document.querySelector('.img-title')
+const closeModal = document.querySelector('.fa-regular.fa-circle-xmark')
+
+//Obtención atributos src para asignar en el lightbox y alt para el título
+images.forEach(image =>{
+    image.addEventListener('click', ()=> {
+        addImage(image.getAttribute('src'), image.getAttribute('alt'))
+    })
+})
+
+//Asignación de src y título al lightbox
+const addImage = (srcImage, altImage)=> {
+    containerImage.classList.toggle('active')
+    imageContainer.classList.toggle('show')
+    imageContainer.src = srcImage
+    imageTitle.innerHTML = altImage
+}
+
+//Agregar clases active y show para abrir/cerrar el lightbox al hacer click en el ícono
+//y mostrar u ocultar la imagen dentro
+closeModal.addEventListener('click', ()=>{
+    containerImage.classList.toggle('active')
+    imageContainer.classList.toggle('show')
+})
+// FIN Código lightbox imágenes //
