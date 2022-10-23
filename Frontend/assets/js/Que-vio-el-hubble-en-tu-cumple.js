84ba1a011cfa476bec4df3bee7203e5c95a8fc4e
romy-rbo <oliva.romina@gmail.com>
2022-10-22 23:09:35 -0300

    Agregado footer con form y validación js

--- Frontend/assets/js/Que-vio-el-hubble-en-tu-cumple.js
+++ Frontend/assets/js/Que-vio-el-hubble-en-tu-cumple.js
@@ -27,3 +27,29 @@ closeModal.addEventListener('click', ()=>{
     imageContainer.classList.toggle('show')
 })
 // FIN Código lightbox imágenes //
+
+// INICIO validación formulario //
+var form = document.getElementById('form-news')
+var nombre = document.getElementById('name')
+var email = document.getElementById('email')
+var error = document.getElementById('form-error-message')
+var sent = document.getElementById('form-sent-message')
+error.style.color = 'red'
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    var mensajeError = []
+
+    if(nombre.value === null || nombre.value === ''){
+        mensajeError.push('Ingresa tu nombre.')
+    }
+    if(email.value === null || email.value === ''){
+        mensajeError.push('Ingresa tu e-mail.')
+    }
+
+    error.innerHTML = mensajeError.join(' ')
+
+    return false;
+})
+// FIN validación formulario //
